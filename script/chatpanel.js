d6c807211096c82f3f8920d800390d8ae2b62098
serenafr <angneszhy@gmail.com>
2015-05-10 14:53:34 -0700

    Show messages in chatHistory panel when 'send' button or 'enter' is pressed

--- script/chatpanel.js
+++ script/chatpanel.js
@@ -6,6 +6,15 @@ function ChatPanel() {
 		'click', 
 		bind(this.onCloseClick, this));
 	this.panelCloseCallBack = null;
+	this.inputBox = document.getElementById('chat-input-box');
+	this.chatHistory = document.getElementById('chat-history');
+	var sendButton = document.getElementById('send-button');
+	sendButton.addEventListener(
+		'click',
+		bind(this.onSendClick, this));
+	this.inputBox.addEventListener(
+		'keyup',
+		bind(this.onEnterUp, this));
 }
 
 ChatPanel.prototype.show = function(name) {
@@ -25,4 +34,31 @@ ChatPanel.prototype.onCloseClick = function() {
 
 ChatPanel.prototype.setPanelCloseCallback = function(callback) {
 	this.panelCloseCallBack = callback;
+}
+
+ChatPanel.prototype.getInputText = function() {
+	return this.inputBox.value;
+}
+
+ChatPanel.prototype.onSendClick = function() {
+	var str = this.getInputText();
+	if(str) {
+		this.setChatHistory(str);
+	}
+	this.inputBox.value = '';
+	this.inputBox.focus();
+}
+
+ChatPanel.prototype.setChatHistory = function(str) {
+	var chatContentNode = document.createTextNode(str);
+	this.chatHistory.element = document.createElement('div');
+	this.chatHistory.element.appendChild(chatContentNode);
+	this.chatHistory.element.className = 'message-me';
+	this.chatHistory.appendChild(this.chatHistory.element);
+}
+
+ChatPanel.prototype.onEnterUp = function(event) {
+	if (event.keyCode == 13) {
+		this.onSendClick();
+	}
 }
